1db3d23bc91b963e71e1373f691a61558071511d
uvsca1 <robokon.uvsca1@gmail.com>
2017-03-04 07:06:11 +0000

    Change pin

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -22,8 +22,8 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(12, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(10000);              // wait for a second
-  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(12, LOW);    // turn the LED off by making the voltage LOW
   delay(10000);              // wait for a second
 }
